a701cf920e206c13c8460896c2b7206ea44b4570
sajjad <sshadkany@gmail.com>
2024-08-16 18:37:38 +0330

    feat: add Choose date page to router

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -4,6 +4,7 @@ import 'package:doctor_app/features/patient/electronic_file/electronic_file.dart
 import 'package:doctor_app/features/patient/home/home.dart';
 import 'package:doctor_app/features/patient/notifications/notifications.dart';
 import 'package:doctor_app/features/patient/profile/profile.dart';
+import 'package:doctor_app/features/patient/reservation/choose_data.dart';
 import 'package:doctor_app/features/patient/search/search.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -32,6 +33,12 @@ final GoRouter router = GoRouter(
       //     },
       //   ),
       // ],
+    ),
+    GoRoute(
+      path: '/chooseDate',
+      builder: (BuildContext context, GoRouterState state) {
+        return  const ChooseData();
+      },
     ),
 
     // GoRoute(
